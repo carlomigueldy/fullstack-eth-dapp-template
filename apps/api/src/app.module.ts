e2b23148ce3df6d5b23802f6284baa6ddf2e8ccf
Carlo Miguel Dy <carlomigueldy@gmail.com>
2023-06-05 19:10:16 +0800

    Add env vars config module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { SupabaseModule } from './supabase/supabase.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, SupabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    AuthModule,
+    UsersModule,
+    SupabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/supabase/supabase.service.ts
+++ apps/api/src/supabase/supabase.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
 
 @Injectable()
 export class SupabaseService {
   client: SupabaseClient;
 
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     this.client = createClient(
-      process.env.SUPABASE_URL,
-      process.env.SUPABASE_KEY,
+      this.configService.get('SUPABASE_URL'),
+      this.configService.get('SUPABASE_KEY'),
     );
   }
 }
